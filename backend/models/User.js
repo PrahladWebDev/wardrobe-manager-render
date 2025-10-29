2d1a7015fc6b9675d1b7c110b9d388c14c3876d3
Prahlad Singh <prahladsingh7512@gmail.com>
2025-10-29 12:31:00 +0530

    Update User.js
    added verifiaction of email

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,14 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
+  verified: { type: Boolean, default: false },
+  verificationToken: String,
+  verificationTokenExpires: Date,
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
   createdAt: { type: Date, default: Date.now },
 });
 
